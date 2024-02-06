9b1d259e96fa41cbd05ddbf85fd0403f3e8a4636
Kajanan <Kajanan15i@gmail.com>
2024-02-06 16:33:59 +0000

    Completed task 1

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public void calculateArea(int length, int width){
+        int area = length * width;
+    }
+
+
+}
