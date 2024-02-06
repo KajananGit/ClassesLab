f445455bf69e35cffb05a87929589656a6923600
Kajanan <Kajanan15i@gmail.com>
2024-02-06 16:46:22 +0000

    Completed Part 2 Task 1

---
+++ src/main/java/Computer.java
@@ -0,0 +1,11 @@
+public class Computer {
+
+    private int storage;
+    private String printer;
+    private boolean isConncected;
+
+    public Computer(int storage){
+        this.storage = storage;
+        this.isConncected = false;
+    }
+}
